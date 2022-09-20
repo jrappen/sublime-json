91949196b6401d0010bc7cc5fb1969bc30a5c65a
Johannes Rappen <jrappen@outlook.com>
2022-09-20 09:24:38 +0200

    [JSON] fix package reloading after update

--- JSON/main.py
+++ JSON/main.py
@@ -7,7 +7,22 @@
 
 from __future__ import annotations                                              # https://docs.python.org/3.8/library/__future__.html
 
+
 import sublime
+
+
+import sys
+
+
+prefix = __package__ + "."
+for module_name in [
+    module_name
+    for module_name in sys.modules
+    if module_name.startswith(prefix) and module_name != __name__
+]:
+    del sys.modules[module_name]
+prefix = None
+
 
 from .src import *
 
